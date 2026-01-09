3dee8ca5ae4b6295d90efc376ce963155fe62ed1
TonyQJH <108768938+TonyQJH@users.noreply.github.com>
2026-01-09 17:33:13 +0800

    update

--- js/render.js
+++ js/render.js
@@ -6,7 +6,7 @@ const get = (id) => document.getElementById(id);
 function renderFavicon() {
     const link = document.createElement('link');
     link.rel = 'icon';
-    link.href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>";
+    link.href = "assets/img/logo/retai_logo_small.png";
     document.head.appendChild(link);
 }
 
